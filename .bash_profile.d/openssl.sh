150d2c8b42ca04f01b26c2d8403f863786fd8c50
Jim Park <jimp@scribd.com>
2023-10-03 11:59:08 -0700

    wsl

--- .bash_profile.d/openssl.sh
+++ .bash_profile.d/openssl.sh
@@ -1,2 +1,3 @@
 export LDFLAGS="${LDFLAGS:+$LDFLAGS }-L/home/linuxbrew/.linuxbrew/opt/openssl@1.1/lib"
-export CPATH="${CPATH:+$CPATH:}/home/linuxbrew/.linuxbrew/opt/openssl@1.1/include"
+export CPPFLAGS="${CPPFLAGS:+$CPPFLAGS }-I/home/linuxbrew/.linuxbrew/opt/openssl@1.1/include"
+export PKG_CONFIG_PATH="${PKG_CONFIG_PATH:+$PKG_CONFIG_PATH:}/home/linuxbrew/.linuxbrew/opt/openssl@1.1/lib/pkgconfig"
