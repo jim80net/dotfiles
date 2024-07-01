5375b20c75c075734aa666b19ac708761865fef6
Jim Park <parks@tesla.com>
2024-07-01 16:33:39 -0700

    update window sizing; add gpuocf alias

--- .zsh.d/git.sh
+++ .zsh.d/git.sh
@@ -1,3 +1,4 @@
 alias gpuoc='git push --set-upstream origin $(git symbolic-ref --short HEAD)'
+alias gpuocf='git push --set-upstream origin $(git symbolic-ref --short HEAD) --force-with-lease'
 alias gpuamend="git commit --amend --no-edit && git push --force-with-lease"
 alias gst="git status"
