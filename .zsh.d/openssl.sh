964d9076bd69ecad2b5703dd80aa510790f1be1b
Jim Park <parks@tesla.com>
2024-07-01 16:34:54 -0700

    openss; rust; teleport

---
+++ .zsh.d/openssl.sh
@@ -0,0 +1,2 @@
+export CFLAGS="-I/opt/homebrew/Cellar/openssl@3/3.3.0/include"
+export LDFLAGS="-L/opt/homebrew/Cellar/openssl@3/3.3.0/lib"
---
+++ .zsh.d/teleport.sh
@@ -0,0 +1,1 @@
+export PATH=${HOME}/w/github.tesla.com/design-technology/teleport-ssh-config/bin:$PATH
