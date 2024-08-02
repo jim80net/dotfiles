1475352d452f53cd51c81e090db648cbab296753
Jim Park <parks@tesla.com>
2024-08-02 10:30:13 -0700

    20240802

---
+++ .vimrc
@@ -0,0 +1,5 @@
+set tabstop=2 softtabstop=2 shiftwidth=2
+set number ruler
+set autoindent smartindent
+syntax enable
+filetype plugin indent on
---
+++ .zsh.d/k8s_export.sh
@@ -0,0 +1,26 @@
+# https://gist.github.com/jim80net/cd27985d72301a9344072b574142aa39
+#
+# Prerequisites:
+# [GNU Parallel](https://www.gnu.org/software/parallel/)
+# [kubectl-neat](https://github.com/itaysk/kubectl-neat)
+# [mikefarah/yq](https://github.com/mikefarah/yq)
+# 
+# Usage:
+# - Copy and paste the below into your active shell session. Alternatively, add to your shell initialization scripts.
+# - kubectl_export <namespace>
+# The folder structure will be created: <namespace>/<kind>/<resource_name>.yaml
+#
+# Inspired from https://www.studytonight.com/post/how-to-list-all-resources-in-a-kubernetes-namespace
+
+
+function k8s_export {
+  NAMESPACE=${1:?"Supply a namespace with this command"}
+  mkdir -p $NAMESPACE
+  kubectl api-resources --verbs=list --namespaced -o name | grep -v "events.events.k8s.io" | grep -v "events" | sort | uniq | parallel --tag --env NAMESPACE -- '\
+    kubectl get {} -n '$NAMESPACE' 2>&1 | grep -vq "No resources found in '$NAMESPACE' namespace" && \
+      mkdir -p '$NAMESPACE'/{} && \
+      kubectl neat get -- {} -n '$NAMESPACE' --ignore-not-found -o yaml | yq --split-exp "\"'$NAMESPACE'/{}/\" + .metadata.name" ".items[]"
+    '
+}
+
+export -f k8s_export >/dev/null
